e7b70d283ae680c72ed95e4c2310194990bf56c7
Fethan Us <usfethan@gmail.com>
2021-11-24 12:17:23 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
+// const routes = require ('');
+//const hbs = exphbs.create({  });
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
-app.get('/'), (req, res) => 
-res.sendFile(path.join(__dirname, '/views/layouts/main.handlebars'))
+
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html')))
+
 
 app.listen(PORT, () => console.log('App listening on port ${PORT}'));
