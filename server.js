173057f5c9071e4c9b9778371cf2833449cb7686
Fethan Us <usfethan@gmail.com>
2021-11-26 10:21:44 -0500

    added path in server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require("./config/connection");
+const path = require('path');
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 const session = require('express-session'); 
